1841007992398fe89b8b1349cb3bfe6f47a95edd
Unjustifiable1 <cupidojustin7@gmail.com>
2024-06-27 11:59:40 +0200

    section margins. cta setup for 2 columns and image

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,16 +33,16 @@ window.addEventListener("scroll", function () {
  * navbar toggle
  */
 
-// const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
-// const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
 
-// navToggleBtn.addEventListener("click", function () {
+navToggleBtn.addEventListener("click", function () {
 
-//   elemToggleFunc(navToggleBtn);
-//   elemToggleFunc(navbar);
-//   elemToggleFunc(document.body);
+  elemToggleFunc(navToggleBtn);
+  elemToggleFunc(navbar);
+  elemToggleFunc(document.body);
 
-// });
+});
 
 
 
